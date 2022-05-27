cbbaa61e525c7a89062a9789b7fed184683f2c54
developer3441 <developer3441@gmail.com>
2022-05-28 02:12:45 +0500

    mongoAtlasServer

--- src/controllers/updateController.js
+++ src/controllers/updateController.js
@@ -2,16 +2,7 @@ const catchAsync = require('../utils/catchAsync');
 const axios = require('axios');
 const schedule = require('node-schedule');
 
-const d1_data = require('../models/d1_data')
-const h1_data = require('../models/h1_data')
-const h4_data = require('../models/h4_data')
-const h6_data = require('../models/h6_data')
-const h12_data = require('../models/h12_data')
-const m1_data = require('../models/m1_data')
-const m3_data = require('../models/m3_data')
-const m5_data = require('../models/m5_data')
-const m15_data = require('../models/m15_data')
-const m30_data = require('../models/m30_data')
+
 var cron = require('node-cron');
 const coins = require('../utils/data')
 
@@ -25,6 +16,8 @@ if (server === 1) {
     data = coins.usdtCoins.slice(100, 200)
 } else if (server === 3) {
     data = coins.usdtCoins.slice(200, 300)
+} else if (server === 4) {
+    data = coins.usdtCoins.slice(300, 400)
 }
 
 
@@ -35,9 +28,9 @@ if (server === 1) {
 module.exports = async () => {
 
 
-    // const timeframes = ['1d', '12h', '6h', '4h', '1h', '30m', '15m', '5m', '3m', '1m']
+    const timeframes = ['1d', '12h', '6h', '4h', '1h', '30m', '15m', '5m', '3m', '1m']
 
-    const timeframes = ['5m', '3m', '1m']
+    // const timeframes = ['5m', '3m', '1m']
 
     let crontime = '* * * * * *'
     let call = '1'
@@ -49,18 +42,13 @@ module.exports = async () => {
 
         for (let index = 0; index < data.length; index++) {
             const el = data[index];
-            cp = el
-            cp = cp.slice(cp.length - 3, cp.length)
 
-            if (timeframe === '1m' && cp === 'BTC') {
-
-            } else {
-                try {
-                    await axios.post(`${process.env.base_link}/api/trading-room-v2/data?coinpair=${el}&call=1&timeframe=${timeframe}&limit=1000`)
-                } catch (error) {
-                    console.log(error)
-                }
+            try {
+                await axios.post(`${process.env.base_link}/api/trading-room-v2/data?coinpair=${el}&call=1&timeframe=${timeframe}&limit=1000`)
+            } catch (error) {
+                console.log(error)
             }
+
 
         }
 
