a8420189ab7f30fc032b9a13e86a68936fa924ff
developer3441 <developer3441@gmail.com>
2022-05-29 16:11:37 +0500

    server4_usdt

--- src/controllers/updateController.js
+++ src/controllers/updateController.js
@@ -6,7 +6,7 @@ const schedule = require('node-schedule');
 var cron = require('node-cron');
 const coins = require('../utils/data')
 
-const server = 1
+const server = 4
 
 let data = coins.usdtCoins.slice(0, 100)
 
